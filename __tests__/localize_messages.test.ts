6472f78c77fe27a9160f228169fd91cf335f92ae
mhaagens <martin.haagensli@gmail.com>
2022-10-14 14:30:14 +0200

    failing test

--- __tests__/localize_messages.test.ts
+++ __tests__/localize_messages.test.ts
@@ -2,12 +2,19 @@ describe("Localize messages", () => {
   it("returns a message in norwegian", () => {
     expect(
       localizeMessage(norwegianBundle, { message: MESSAGE["GENERAL_ERROR"] })
-    ).toBe(norwegianBundle[MESSAGE["GENERAL_ERROR"]]);
+    ).toBe("Noe gikk galt");
+  });
+
+  it("returns network error in english", () => {
+    expect(
+      localizeMessage(englishBundle, { message: MESSAGE["NETWORK_ERROR"] })
+    ).toBe("Something went wrong");
   });
 });
 
 enum MESSAGE {
   GENERAL_ERROR = "GENERAL_ERROR",
+  NETWORK_ERROR = "NETWORK_ERROR",
 }
 
 type Bundle = {
@@ -17,6 +24,8 @@ type Bundle = {
 const norwegianBundle: Bundle = {
   GENERAL_ERROR: "Noe gikk galt",
 };
+
+const englishBundle: Bundle = {};
 
 function localizeMessage(
   norwegianBundle: Bundle,
