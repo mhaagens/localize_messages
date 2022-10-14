e0ae220e6974c95389d81fe01f753063569fb61b
mhaagens <martin.haagensli@gmail.com>
2022-10-14 14:06:53 +0200

    GENERAL_ERROR fails

---
+++ __tests__/localize_messages.test.ts
@@ -0,0 +1,13 @@
+describe("Localize messages", () => {
+  it("returns a message in norwegian", () => {
+    expect(localizeMessage(norwegianBundle, { message: "GENERAL_ERROR" })).toBe(
+      "Noe gikk galt"
+    );
+  });
+});
+
+const norwegianBundle = {};
+
+function localizeMessage(norwegianBundle: any, arg1: { message: string }): any {
+  throw new Error("Function not implemented.");
+}
